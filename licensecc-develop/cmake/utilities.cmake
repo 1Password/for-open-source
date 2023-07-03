315a65af79fc8c17bc213fffe168048af20c3242
HaiTX3004 <100550537+HaiTX3004@users.noreply.github.com>
2023-07-03 14:18:24 +0700

    Delete licensecc-develop directory

--- licensecc-develop/cmake/utilities.cmake
+++
@@ -1,9 +0,0 @@
-
-MACRO(EVAL_CONDITION name)
-   IF(${ARGN})
-     SET(${name} 1)
-   ELSE(${ARGN})
-     SET(${name} 0)
-   ENDIF(${ARGN})
-ENDMACRO(EVAL_CONDITION)
-
--- licensecc-develop/scripts/windows_download_boost.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM build and cache boost on Windows N.B. if no output for 10 minutes it's killed by travis. 
-if not exist "C:/local/boost/libs" (
-        echo "Boost not cached, compiling it"
-		wget -q -O boost.exe https://versaweb.dl.sourceforge.net/project/boost/boost-binaries/1.71.0/boost_1_71_0-msvc-14.1-64.exe
-		echo "Boost downloaded"
-		boost.exe /SILENT /DIR=C:\local\boost 
-		echo "Boost installed"
-) else echo "Boost already installed"

2946aa587616987f3bceaf1a54c04075ccab17c1
Jody Heavener <jody.heavener@agilebits.com>
2024-03-13 08:40:31 -0700

    Set up processor script for validation logic

--- script/main.go
+++ script/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func printUsageAndExit() {
+	fmt.Println("Usage: ./processor <validate>")
+	os.Exit(1)
+}
 
 func main() {
-	fmt.Println("Hello world")
+	if len(os.Args) != 2 {
+		printUsageAndExit()
+	}
+
+	command := os.Args[1]
+	switch command {
+	case "validate":
+		// handle app validation
+	default:
+		fmt.Printf("Invalid command: %s\n", command)
+		printUsageAndExit()
+	}
 }

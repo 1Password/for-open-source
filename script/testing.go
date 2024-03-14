00e85fa8ae6f9f353b9ee48b9b3d622da5fc891b
Jody Heavener <jody.heavener@agilebits.com>
2024-03-14 14:14:59 -0700

    Add testing instruments and example issue JSON

---
+++ script/testing.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/go-github/github"
+)
+
+var testIssueName string
+
+func isTestingIssue() bool {
+	return testIssueName != ""
+}
+
+func testMessage(message string, data ...interface{}) {
+	if isTestingIssue() {
+		fmt.Printf("[DEBUG] %s\n", message)
+
+		for _, entry := range data {
+			fmt.Println(entry)
+		}
+	}
+}
+
+func getTestIssue() github.Issue {
+	data, err := os.ReadFile(fmt.Sprintf("./script/test-issues/%s.json", testIssueName))
+	if err != nil {
+		log.Fatalf("Unable to read the file: %v", err)
+	}
+
+	var issue github.Issue
+	err = json.Unmarshal(data, &issue)
+
+	if err != nil {
+		log.Fatalf("Unable to unmarshal JSON: %v", err)
+	}
+
+	return issue
+}
